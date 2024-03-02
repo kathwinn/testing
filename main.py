c484db8da33b2e359451578e4dccb15c67d784cf
kathwinn <161905419+kathwinn@users.noreply.github.com>
2024-03-02 12:05:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+def read_main(token: str = None):
+   if token != "katherine":
+      raise HTTPException(status_code=401, detail="Unauthorized")
+   return {"message": "Hello, World!"} 
